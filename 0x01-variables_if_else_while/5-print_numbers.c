8c35e499fbe71e4e6ece14a935fe4b44111bdd3b
donkelvo <elejeokechukwu@gmail.com>
2022-12-10 00:29:58 -0800

    C variables Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - return numbers
+ * Return: return 0 (Successful)
+ */
+
+int main(void)
+{
+	int i;
+
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+
+
+	return (0);
+
+}

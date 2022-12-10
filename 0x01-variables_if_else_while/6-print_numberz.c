c149903df34fbb87ed331d97bd7293ad477efc52
donkelvo <elejeokechukwu@gmail.com>
2022-12-10 00:40:55 -0800

    C variables Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - return numbers
+ * Return: return 0 (Successful)
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	putchar('\n');
+
+	return (0);
+}

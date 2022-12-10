5ac21e9d839591716de61ec0a0dec63050e73a19
donkelvo <elejeokechukwu@gmail.com>
2022-12-10 01:17:56 -0800

    C variables Task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
 /**
  * main - reverse alphabets
  * Return: return 0 (Successful)
  */
-
 int main(void)
 {
 	char low;
-
 	for (low = 'z'; low >= 'a'; low--)
 		putchar(low);
 		putchar('\n');
-
 	return (0);
 }
